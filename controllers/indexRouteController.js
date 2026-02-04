727379fd9096e15eaba49f4636530b0c886d789a
Johnny Cage <thespammingbott@gmail.com>
2026-02-04 11:54:09 +0100

    Fix index route to await getGames and pass actual data to template

--- controllers/indexRouteController.js
+++ controllers/indexRouteController.js
@@ -1,3 +1,11 @@
-exports.indexRouteGet = (req, res) => {
-    res.render("home", {})
-}
+const queries = require("../data/queries");
+
+exports.indexRouteGet = async (req, res) => {
+  try {
+    const allGames = await queries.getGames();
+    res.render("home", { data: allGames });
+  } catch (err) {
+    console.error("Failed to fetch games", err)
+    res.status(500).send("Internal Server error")
+  }
+};
