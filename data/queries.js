396fb65bf603f79a8da5642b5aafe6b690bc62d1
Johnny Cage <thespammingbott@gmail.com>
2026-02-04 15:40:56 +0100

    Update queries.js to aggregate genres for each game correctly

--- data/queries.js
+++ data/queries.js
@@ -1,7 +1,17 @@
 const pool = require("./pool")
 
-async function getGames() {
-    const { rows } = await pool.query("SELECT * FROM games")
+async function getGamesWithPublishers() {
+    const { rows } = await pool.query(`
+    SELECT games.id, 
+    games.name,
+    publishers.name AS publisher,
+    ARRAY_AGG(genres.name) AS genres 
+    FROM games
+    JOIN publishers ON games.publisher_id = publishers.id
+    LEFT JOIN game_genres ON games.id = game_genres.game_id     
+    LEFT JOIN genres ON game_genres.genre_id = genres.id
+    GROUP BY games.id, publishers.name;
+    `)
     return rows;
 }
 
@@ -21,7 +31,7 @@ async function getPlatforms() {
 }
 
 module.exports = {
-    getGames,
+    getGamesWithPublishers,
     getGenres,
     getPublishers,
     getPlatforms,
