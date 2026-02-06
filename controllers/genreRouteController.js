cda8ded8ecaacb28cbac4327a10a78d3cf95c50e
Johnny Cage <thespammingbott@gmail.com>
2026-02-06 16:04:01 +0100

    implement getGenres function & import queries

--- controllers/genreRouteController.js
+++ controllers/genreRouteController.js
@@ -1,3 +1,6 @@
-exports.genreRouteGet = (req, res) => {
-    res.render("genre", {})
+const queries = require("../data/queries")
+
+exports.genreRouteGet = async (req, res) => {
+    const getGenres = await queries.getGenres()
+    res.render("genre", { data: getGenres })
 }
