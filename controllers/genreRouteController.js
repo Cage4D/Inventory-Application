2b6e26ec5018cde08b2d9e92c382cfbaf46358c7
Johnny Cage <thespammingbott@gmail.com>
2026-02-06 17:16:30 +0100

    Replaced manual body rendering + ejs.renderFile with direct res.render()

--- controllers/genreRouteController.js
+++ controllers/genreRouteController.js
@@ -2,5 +2,5 @@ const queries = require("../data/queries")
 
 exports.genreRouteGet = async (req, res) => {
     const getGenres = await queries.getGenres()
-    res.render("genre", { data: getGenres })
+    res.render("pages/genre-content", { data: getGenres })
 }
--- controllers/indexRouteController.js
+++ controllers/indexRouteController.js
@@ -3,7 +3,7 @@ const queries = require("../data/queries");
 exports.indexRouteGet = async (req, res) => {
   try {
     const allGames = await queries.getGamesWithPublishers();
-    res.render("home", { data: allGames });
+    res.render("pages/home-content", { data: allGames })
   } catch (err) {
     console.error("Failed to fetch games", err)
     res.status(500).send("Internal Server error")
