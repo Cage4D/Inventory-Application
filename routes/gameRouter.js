4dea61b7572926c9d5799976f3d855c6cd8e945e
Johnny Cage <thespammingbott@gmail.com>
2026-02-06 21:23:30 +0100

    install express-validator dependency

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express")
 const gameRouter = Router()
 const gameRouterController = require("../controllers/gameRouterController")
 
-gameRouter.get("/", gameRouterController.gameRouterGet)
-
+gameRouter.get("/", gameRouterController.gameRouterRedirect)
+gameRouter.get("/new", gameRouterController.gameRouterGet)
+gameRouter.post("/addGame", gameRouterController.gameRouterPost)
 module.exports = gameRouter;
