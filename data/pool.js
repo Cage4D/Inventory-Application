bad8c9ba2809045677bdd174204c0094326f2186
Johnny Cage <thespammingbott@gmail.com>
2026-02-07 12:40:03 +0100

    replace CONNECTION_URL with DATABASE_URL

--- data/pool.js
+++ data/pool.js
@@ -2,5 +2,6 @@ const { Pool } = require("pg")
 require("dotenv").config()
 
 module.exports = new Pool({
-    connectionString: process.env.CONNECTION_STRING
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 })
